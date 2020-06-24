1f1d6a8830e6fefe67b90ba17ff60ef2f3749fa6
sailor <18962364008@163.com>
2020-06-24 15:28:00 +0800

    增加了business模块，并增加初始启动代码

---
+++ business/src/main/java/com/course/business/controller/TestController.java
@@ -0,0 +1,29 @@
+package com.course.business.controller;
+
+import com.course.server.domain.Test;
+import com.course.server.service.TestService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ * class:TestController
+ * author: sailor lee
+ * mail:18962364008@163.com
+ * description:
+ */
+
+@RestController
+public class TestController {
+
+    @Resource
+    private TestService testService;
+
+    //访问test地址
+    @RequestMapping("/test")
+    public List<Test> test(){
+        return testService.list();
+    }
+}
