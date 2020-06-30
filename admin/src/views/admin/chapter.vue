224c179a52a5bbee29b8c82fb0c1c16fe25d9758
sailor <18962364008@163.com>
2020-06-30 16:43:19 +0800

    制作toast组件，内部用的是sweetalert

--- admin/src/views/admin/chapter.vue
+++ admin/src/views/admin/chapter.vue
@@ -192,6 +192,7 @@
                     if (resp.success){
                         $("#form-modal").modal("hide");
                         _this.list(1);
+                        toast.success("保存成功！");
                     }
                 })
             },
@@ -212,11 +213,7 @@
                             let resp = response.data;
                             if (resp.success){
                                 _this.list(1);
-                                Swal.fire(
-                                    '删除成功',
-                                    'Your file has been deleted.',
-                                    'success'
-                                )
+                                toast.success("删除成功！")
                             }
                         })
 
