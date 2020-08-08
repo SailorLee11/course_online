a686b1871c323cc4abfb4c60c0ccd763ed593f90
sailor <18962364008@163.com>
2020-08-08 09:59:55 +0800

    保存节时，更新课程总时长 增加自定义mapper

--- server/src/main/java/com/course/server/service/SectionService.java
+++ server/src/main/java/com/course/server/service/SectionService.java
@@ -30,6 +30,9 @@ public class SectionService {
     @Resource
     private SectionMapper sectionMapper;
 
+    @Resource
+    private CourseService courseService;
+
     public void list(SectionPageDto sectionPageDto){
         PageHelper.startPage(sectionPageDto.getPage(),sectionPageDto.getSize());
         SectionExample sectionExample = new SectionExample();
@@ -63,6 +66,7 @@ public class SectionService {
         } else {
             this.update(section);
         }
+        courseService.updateTime(sectionDto.getCourseId());
     }
 
     private void insert(Section section){
